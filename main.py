af6914a9f86bff0039e3de4667229191eb472598
Tsubasa SEKIGUCHI <ts@tinykitten.me>
2021-04-01 01:52:11 +0900

    間引き

--- main.py
+++ main.py
@@ -1,22 +1,44 @@
 #!/usr/bin/python
 
+import asyncio
+from time import sleep
 import simpleaudio
 import paho.mqtt.client as mqtt
 import os
 from dotenv import load_dotenv
+import concurrent.futures
 
 load_dotenv()
+
+played = False
+DEBOUNCE_DELAY = os.environ.get("DEBOUNCE_DELAY")
+
+
+def debounce(delay):
+    global played
+    while True:
+        played = False
+        sleep(delay)
+
+
+async def debounce_async(loop, executor):
+    global played
+    loop.run_in_executor(executor, debounce, DEBOUNCE_DELAY)
 
 
 def on_connect(client, data, flags, rc):
     client.subscribe("praise/count", 1)
+    loop = asyncio.get_event_loop()
+    executor = concurrent.futures.ThreadPoolExecutor(max_workers=1)
+    loop.run_until_complete(debounce_async(loop, executor))
 
 
 def on_message(client, data, msg):
-    print("Praise received!")
-    wav_obj = simpleaudio.WaveObject.from_wave_file("osaka-loopline.wav")
-    play_obj = wav_obj.play()
-    play_obj.wait_done()
+    global played
+    if played == False:
+        wav_obj = simpleaudio.WaveObject.from_wave_file("osaka-loopline.wav")
+        wav_obj.play()
+        played = True
 
 
 client = mqtt.Client()
